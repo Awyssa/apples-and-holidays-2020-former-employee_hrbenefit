93708bb6c7c7418659d8a79b7e7e7ad4c51d52dc
Michael Ernest <m.hendersun@gmail.com>
2020-10-08 15:46:02 +0000

    Automatically backed up by Learn

--- code.rb
+++ code.rb
@@ -23,4 +23,12 @@ def all_winter_holiday_supplies(holiday_hash)
   puts winter_supplies
 end
 
-all_winter_holiday_supplies(holiday_hash)
+
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    end
+  end
+end
