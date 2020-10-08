937ab643c3a100c168fcf0943072214d700a370f
Michael Ernest <m.hendersun@gmail.com>
2020-10-08 15:44:58 +0000

    Automatically backed up by Learn

--- code.rb
+++ code.rb
@@ -1,18 +1,16 @@
-let(:holiday_supplies) {
-  {
-    :winter => {
-      :christmas => ["Lights", "Wreath"],
-      :new_years => ["Party Hats"]
-    },
-    :summer => {
-      :fourth_of_july => ["Fireworks", "BBQ"]
-    },
-    :fall => {
-      :thanksgiving => ["Turkey"]
-    },
-    :spring => {
-      :memorial_day => ["BBQ", "Watermelon"] #MH: Added "Watermelon"
-    }
+holiday_hash = {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
   }
 }
 
