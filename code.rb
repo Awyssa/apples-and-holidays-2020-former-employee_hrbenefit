560abd6321774dd58f29819d3b3c57156db9998f
Michael Ernest <m.hendersun@gmail.com>
2020-10-08 15:49:57 +0000

    Automatically backed up by Learn

--- code.rb
+++ code.rb
@@ -20,24 +20,24 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.map do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holidays, supplies|
-      puts " #{holiday.to_s.split('_').map {|w|} w.capitalize}.join(" ") }: #{supplies.join(",")}"
-  end
-end
-
-
-
-
-
-
-
-
-
-
-def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holidays|
-    puts "#{season.capitalize}:"
-    holidays.each do |holiday, supplies|
       puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
-    end
   end
 end
+
+
+
+
+
+
+
+
+
+
+# def all_supplies_in_holidays(holiday_hash)
+#   holiday_hash.each do |season, holidays|
+#     puts "#{season.capitalize}:"
+#     holidays.each do |holiday, supplies|
+#       puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+#     end
+#   end
+# end
