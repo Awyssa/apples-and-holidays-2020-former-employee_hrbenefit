d977200dac9fe32581bc80c7250aa43091fadaf0
Michael Ernest <m.hendersun@gmail.com>
2020-10-08 15:44:06 +0000

    Automatically backed up by Learn

--- code.rb
+++ code.rb
@@ -1,7 +1,26 @@
+let(:holiday_supplies) {
+  {
+    :winter => {
+      :christmas => ["Lights", "Wreath"],
+      :new_years => ["Party Hats"]
+    },
+    :summer => {
+      :fourth_of_july => ["Fireworks", "BBQ"]
+    },
+    :fall => {
+      :thanksgiving => ["Turkey"]
+    },
+    :spring => {
+      :memorial_day => ["BBQ", "Watermelon"] #MH: Added "Watermelon"
+    }
+  }
+}
+
+
 def all_winter_holiday_supplies(holiday_hash)
   winter_supplies = []
   holiday_hash[:winter].map do |holiday, supply|
     winter_supplies << holiday[supply]
   end
-  winter_supplies
+  puts winter_supplies
 end
